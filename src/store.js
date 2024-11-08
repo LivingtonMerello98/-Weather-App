2957474df16b1867d03f4261a51e51a45fe4aefa
LivingtonMerello98 <livington.merello@gmail.com>
2024-11-08 02:46:30 +0100

    [UPD] Store updated to handle the weather data,  errors and loading with methods

--- src/store.js
+++ src/store.js
@@ -3,4 +3,22 @@ import { reactive } from 'vue';
 export const store = reactive({
     apiUrl: import.meta.env.VITE_API_URL,
     geocodeUrl: `${import.meta.env.VITE_GEOCODE_URL}?appid=${import.meta.env.VITE_API_KEY}`,
+
+    weatherData: null, // memorizza dati meteo generali
+    loading: false, // gestisce stato di caricamento
+    error: null,
+
+    // metodo per aggiornare dati meteo
+    setWeatherData(data) {
+        this.weatherData = data;
+    },
+
+    // metodo per gestire gli errori
+    setError(error) {
+        this.error = error;
+    },
+
+    setLoading(isLoading) {
+        this.loading = isLoading;
+    },
 });
