ae3a4c03b525c36ca0f78bf9f1644571132ff721
LivingtonMerello98 <livington.merello@gmail.com>
2024-11-08 18:39:01 +0100

    [UPD] layout App.vue optimization

--- src/App.vue
+++ src/App.vue
@@ -20,38 +20,33 @@ export default {
     <section class="bg-gray-100 min-h-screen">
       <!-- Header -->
       <HeaderComponent />
-  
-      <div class="flex flex-col md:flex-row p-4 gap-6">
-        <!-- Current Weather -->
-        <div class="w-full md:w-2/5 bg-blue-950 p-6 rounded-lg">
-          <CurrentWeather />
-        </div>
-  
-        <!-- Daily Forecast -->
-        <div class="w-full md:w-1/3 bg-green-500 p-6 rounded-lg">
-          <DailyForecast />
+
+      <!-- Current Weather -->
+      <div class="flex flex-col p-4 gap-4">
+        <div class="w-full bg-blue-950 rounded-lg">
+            <CurrentWeather />
         </div>
       </div>
 
       <!-- Hourly Forecast -->
       <div class="flex flex-col p-4 gap-4">
-        <div class="w-full bg-gray-300 p-6 rounded-lg">
-            <HourlyForecast />
+        <div class="w-full bg-gray-300 rounded-lg">
+          <HourlyForecast />
+        </div>
+      </div>
+
+      <!-- Hourly Forecast -->
+      <div class="flex flex-col p-4 gap-4">
+        <div class="w-full bg-gray-300 rounded-lg">
+          <DailyForecast />
         </div>
       </div>
   
-      <div class="flex flex-col p-4 gap-4">
-        <!-- Minutely Forecast -->
-        <div class="w-full bg-gray-300 p-6 rounded-lg">
-          <MinutelyForecast />
-        </div>
-      </div>
     </section>
   </template>
   
 
 <style lang="scss" scoped>
-/* Tailwind classes for responsive layout */
 
  
 </style>
