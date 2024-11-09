1988142073c02b4837ea9ba79c51e4c3a6f77345
LivingtonMerello98 <livington.merello@gmail.com>
2024-11-09 20:45:06 +0100

    [UPD] DailyForecast layuot text size update

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ export const store = reactive({
     apiUrl: import.meta.env.VITE_API_URL,
     geocodeUrl: `${import.meta.env.VITE_GEOCODE_URL}?appid=${import.meta.env.VITE_API_KEY}`,
 
+
     // Stato
     state: {
         weatherData: null,
