5f5178941a64c7104c36a01083afa1801bf90118
LivingtonMerello98 <livington.merello@gmail.com>
2024-11-11 23:28:30 +0100

    [FIX] horizzontal scrollbar removed from dailyForecast

--- src/components/DailyForecast.vue
+++ src/components/DailyForecast.vue
@@ -21,23 +21,28 @@ export default {
     <h3 class="text-m font-semibold mb-5">10 Day Forecast</h3>
     <p v-if="!dailyWeather.length" class="text-blue-500">Daily Forecast not available.</p>
 
-    <div v-else class="flex gap-2 overflow-x-auto">
-
-      <!-- previsiomi giornaliere -->
+    <div v-else class="forecast-container">
+      <!-- Previsioni giornaliere -->
       <div v-for="(day, index) in dailyWeather" :key="index" class="bg-gray-900 text-center rounded-lg p-6" style="min-width: 150px;">
         <p class="font-semibold text-xs">{{ formatDate(day.dt) }}</p>
         <img :src="'https://openweathermap.org/img/wn/' + day.weather[0].icon + '@2x.png'" alt="Weather icon" class="mx-auto my-2 w-15 h-15" />
         <p class="text-xs mb-3">{{ day.weather[0].description }}</p>
         <p class="text-xs"><strong>{{ (day.temp.day - 273.15).toFixed(1) }}°C</strong></p>
       </div>
-
     </div>
 
   </div>
 </template>
 
-
-<style lang="scss" scoped>
-
+<style scoped>
+.forecast-container {
+  display: flex;
+  gap: 0.5rem;       
+  overflow-x: auto;  
+  padding-bottom: 1rem; 
+  scrollbar-width: none; /* Nasconde la barra su Firefox */
+}
+.forecast-container::-webkit-scrollbar {
+  display: none; /* Nasconde la barra su Chrome, Safari e Edge */
+}
 </style>
-
